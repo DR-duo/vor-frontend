f3c272d1cd5d8c87b27f0b43a0ade856f1da813b
DC <fma727@hotmail.com>
2019-11-14 17:47:39 -0500

    temporary solution to getting dimension of overlay

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -7,30 +7,59 @@ export default class Overlay extends React.Component {
   constructor() {
     super();
     this.state = {
+      finishedLoading: false,
       lastClick: {
         x: null,
         y: null
+      },
+      windowSize: {
+        x: 0,
+        y: 0
       }
     };
     this.api = new RuneterraAdapter();
   }
 
+  componentDidMount() {
+    this.setState({ finishedLoading: true });
+  }
+
   handleClick(event) {
     const x = event.clientX;
     const y = event.clientY;
-    this.api.getCardAtCord(x, y).then(card => {
-      console.log(card);
-    });
+
+    this.setState(
+      {
+        windowSize: {
+          x: document.getElementById("vor-overlay").clientWidth,
+          y: document.getElementById("vor-overlay").clientHeight
+        }
+      },
+      () => {
+        this.api.getCardAtCord(x, y).then(card => {
+          console.log(card);
+        });
+      }
+    );
   }
 
   render() {
-    return (
-      <div
-        style={{ height: "100%", width: "100%" }}
-        onClick={event => this.handleClick(event)}
-      >
-        Last clicked:
-      </div>
-    );
+    if (this.state.finishedLoading) {
+      return (
+        <div
+          id="vor-overlay"
+          ref={el => (this.container = el)}
+          style={{ height: "100%", width: "100%" }}
+          onClick={event => this.handleClick(event)}
+        >
+          Window Size X: {this.state.windowSize.x}
+          Window Size Y: {this.state.windowSize.y}
+        </div>
+      );
+    } else {
+      return (
+        <div id="vor-overlay" style={{ height: "100%", width: "100%" }}></div>
+      );
+    }
   }
 }
